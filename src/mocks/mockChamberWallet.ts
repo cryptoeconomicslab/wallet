00b4489fd0fce6694231aa3bb367a43117c91b0c
tkmct <1220t.takamichi@gmail.com>
2019-02-05 04:16:37 +0900

    fix

--- src/mocks/mockChamberWallet.ts
+++ src/mocks/mockChamberWallet.ts
@@ -1,19 +1,18 @@
 // mock chamber wallet instance
 // remove this file when chamber-wallet is available
 
-import { BigNumber } from 'ethers/utils'
-import { TransactionReceipt } from 'ethers/providers'
 import delay from '../utils/delay'
 
 // mock chamber wallet instance
 export class MockChamberWallet {
-  private balance: BigNumber = new BigNumber(100)
+  private balance: number = 100
 
-  public getBalance(): BigNumber {
+  public getBalance(): number {
     return this.balance
   }
 
-  public async deposit(): Promise<TransactionReceipt> {
+  // returns TransactionReceipt
+  public async deposit(): Promise<any> {
     await delay(1500)
 
     // test data from https://github.com/ethereum/wiki/wiki/JavaScript-API#example-43
@@ -25,8 +24,8 @@ export class MockChamberWallet {
         '0xef95f2f1ed3ca60b048b4bf67cde2195961e0bba6f70bcbea9a2c4e133e34b46',
       blockNumber: 3,
       contractAddress: '0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b',
-      cumulativeGasUsed: new BigNumber(314159),
-      gasUsed: new BigNumber(30234),
+      cumulativeGasUsed: 314159,
+      gasUsed: 30234,
       logs: [],
       status: 1,
       byzantium: false
