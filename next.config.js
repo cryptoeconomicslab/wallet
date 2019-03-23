d94b63c633f32ea17a6312c04c51747253048bb8
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-03-23 14:02:00 +0900

    update deps and mod next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const webpack = require('webpack')
 const withTypescript = require('@zeit/next-typescript')
 const Dotenv = require('dotenv-webpack')
 
 module.exports = withTypescript({
   webpack(config, options) {
     config.plugins.push(new Dotenv())
+    config.plugins.push(new webpack.NormalModuleReplacementPlugin(/^mqtt$/, "mqtt/dist/mqtt.js"))
+
 
     // need this line to avoid compilation error of ethers.js
     config.externals = [{ xmlhttprequest: 'XMLHttpRequest' }]
